a953b3328e3617ee0d4a47bb32f51de6ea685e12
Matheus Soares <100249571+matheusvoliveira@users.noreply.github.com>
2023-05-26 10:40:30 -0300

    Update password_generator.py

--- password_generator.py
+++ password_generator.py
@@ -1,53 +1,31 @@
 import random
+
+
 class Password:
     def __init__(self):
-        self.caracteres = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz.0123456789"
+        self.caracteres = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789`~!@#$%^&*()-_=+[{]}|;:',<.>/?"
 
-    def randomy(self):
+    def multiple_character(self):
         list = []
-        q = int(input('How many character would you like: '))
-        for x in range(q):
+        for x in range(10):
             password = random.choice(self.caracteres)
             list.append(password)
         full_password = ''.join(list)
         return full_password
 
     def multiple_password(self):
-        quantity = int(input('Quantity: '))
+        quantity = int(input('How many passwords would you like: '))
         all_passwords = []
+        print('\nPASSWORDS: \n')
         for x in range(quantity):
-            all_passwords.append(self.randomy())
+            all_passwords.append(self.multiple_character())
         for password in all_passwords:
             print(password)
 
-    def __str__(self):
-        return f'PASSWORDS: \n{self.multiple_password()}'
 
-test = Password()
-test.multiple_password()import random
-class Password:
-    def __init__(self):
-        self.caracteres = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz.0123456789"
-
-    def randomy(self):
-        list = []
-        q = int(input('How many character would you like: '))
-        for x in range(q):
-            password = random.choice(self.caracteres)
-            list.append(password)
-        full_password = ''.join(list)
-        return full_password
-
-    def multiple_password(self):
-        quantity = int(input('Quantity: '))
-        all_passwords = []
-        for x in range(quantity):
-            all_passwords.append(self.randomy())
-        for password in all_passwords:
-            print(password)
-
-    def __str__(self):
-        return f'PASSWORDS: \n{self.multiple_password()}'
-
-test = Password()
-test.multiple_password()
+if __name__ == '__main__':
+    test = Password()
+    print(20*'-')
+    print('PASSWORD GENERATOR')
+    print(20*'-')
+    test.multiple_password()
